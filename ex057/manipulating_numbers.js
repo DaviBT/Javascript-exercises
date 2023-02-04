f8177a623266f74c9227174d57c511fd8d0fd8ba
Davi Torres <100081569+DaviBT@users.noreply.github.com>
2023-02-04 19:18:20 -0300

    manipulating nubers and strings

---
+++ ex057/manipulating_numbers.js
@@ -0,0 +1,6 @@
+let numbers = 123
+let string = String(numbers)
+
+console.log(string + 7)
+
+// result: 1237
---
+++ ex057/manipulating_string.js
@@ -0,0 +1,6 @@
+let string = "123"
+let numbers = Number(string)
+
+console.log(numbers + 8)
+
+//result: 131
