22f384515910a0685567491f571b8424501f0940
Davi Torres <100081569+DaviBT@users.noreply.github.com>
2023-02-05 21:46:11 -0300

    manipulating strings
    manipulating strings using toUpperCase and to LowerCase functions.

---
+++ ex060/manipulating_strings.js
@@ -0,0 +1,6 @@
+let string = "Hello, this is a JavaScript code made up with node.js"
+console.log(string.toLowerCase())
+// result: hello, this is a javascript code made up with node.js
+
+console.log(string.toUpperCase())
+// result: HELLO, THIS IS A JAVASCRIPT CODE MADE UP WITH NODE.JS
