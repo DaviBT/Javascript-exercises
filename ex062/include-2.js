e3c751dd2e08e462048c0f71735cd1b25141ad89
Davi Torres <100081569+DaviBT@users.noreply.github.com>
2023-02-07 11:22:36 -0300

    includes prototype

---
+++ ex062/include-2.js
@@ -0,0 +1,6 @@
+// find the 3th element of the array using JS, ans say if it's false or true
+
+let array = ["Hello", "I'm", "using", "JavaScript"]
+console.log(array.includes("using"))
+
+// result: true
---
+++ ex062/include.js
@@ -0,0 +1,6 @@
+// find the word "dreams" in the sentence using JS
+
+let str = "I want to reach my dreams!"
+console.log(str.includes("dreams")) // true or false
+
+// result: true
