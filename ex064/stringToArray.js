4e8b1465badbdbc79cc4137d35f1426ce3e28d4f
Davi Torres <100081569+DaviBT@users.noreply.github.com>
2023-02-07 22:24:26 -0300

    transform a string into an array

---
+++ ex064/stringToArray.js
@@ -0,0 +1,13 @@
+// transform each letter from an atring into an string element
+
+let word = "JavaScript"
+
+console.log(Array.from(word))
+
+/* 
+result: [
+  'J', 'a', 'v', 'a',
+  'S', 'c', 'r', 'i',
+  'p', 't'
+] 
+*/
