00ef9f365b6c858bbaaccfbe21c3e564973f03fd
DaviBT <davi.cesarbt@gmail.com>
2023-05-14 20:20:08 -0300

    slplice function, getting certain values of the array

---
+++ manipulating-arrays/ex006.js
@@ -0,0 +1,9 @@
+// deleting certaing values of the array
+
+let workers = ["Jones", "Max", "David", "Lewis"]
+
+workers.splice(1, 2) // (index of the starting value I want to delete, number of values I want to delete)
+
+console.log(workers)
+
+// output: ["Jones", "Lewis"]
