de55efdb04613f883b262077f6bf51e9430a409c
DaviBT <davi.cesarbt@gmail.com>
2023-05-14 20:00:43 -0300

    getting values of the array using slice

---
+++ manipulating-arrays/ex005.js
@@ -0,0 +1,9 @@
+// selecting values of the array
+
+let workers = ["Jones", "Max", "David", "Lewis"]
+
+workers.slice(1,3) // making a slice from the item number 1 untill the item nuber 3
+
+console.log(workers)
+
+// output: ["Max", "David", "Lewis"]
