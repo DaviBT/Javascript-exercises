0f1c70ce82a72b7f247bb55b6f5534ee1d252717
DaviBT <davi.cesarbt@gmail.com>
2023-05-14 22:16:14 -0300

    operator procedence

---
+++ expressions-and-operators/ex007.js
@@ -0,0 +1,15 @@
+// Operator procedence from top to below
+
+// * grouping                      ( )
+// * negation and increment         ! ++ --
+// * multiplication and division    * /
+// * addition and subtraction       + -
+// * relational                     < <= > >=
+// * equality                       == != === !==
+// * AND                            &&
+// * OR                             ||
+// * conditional                    ?:
+// * assignment                     = += -= *= %=
+
+calculus = (5 + 5) * 2
+console.log(calculus) // output: 20
