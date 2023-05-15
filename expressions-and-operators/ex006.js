7980e6a43cc4afb534c9c6a67a8c7f9b1bb73499
DaviBT <davi.cesarbt@gmail.com>
2023-05-14 22:10:38 -0300

    testing the TRUTHY value using ternary operator

---
+++ expressions-and-operators/ex006.js
@@ -0,0 +1,15 @@
+/* 
+    TRUTHY
+	All the values below would be considered as true in a boolean
+    {}
+    []
+    1
+    3.23
+    "0"
+    "false"
+    -1
+    Infinity
+    -Infinity
+*/
+
+console.log( Infinity ? 'true' : 'false' ) // output: true
