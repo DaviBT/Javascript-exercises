78b91cf54f034723f7de83e9d34c6382e638c41b
DaviBT <davi.cesarbt@gmail.com>
2023-05-14 22:08:20 -0300

    testing the FALSY value using ternary operator

---
+++ expressions-and-operators/ex005.js
@@ -0,0 +1,12 @@
+/*
+    FALSY
+	All the values below would be considered false in a boolean.
+    0
+    -0
+    ""
+    null
+    undefined
+    NaN
+*/
+
+console.log( NaN ? 'true' : 'false' ) // output: false
