f73fb652bd71cf04338154b8d9115e1a455649a2
unknown <davi.cesarbt@gmail.com>
2023-06-16 10:52:30 -0300

    prettier applied

--- employees-exercise/employees-poo.js
+++ employees-exercise/employees-poo.js
@@ -7,51 +7,55 @@ class person {
 }
 
 class employee extends person {
-    constructor(name, age, job, salary) {
-        super(name, age);
-        this.job = job;
-        this.salary = salary;
+  constructor(name, age, job, salary) {
+    super(name, age);
+    this.job = job;
+    this.salary = salary;
   }
 }
 
 class programer extends employee {
   constructor(name, job, salary, programingLanguage) {
-    super(name, job, salary)
+    super(name, job, salary);
     this.programingLanguage = programingLanguage;
   }
 
-  toPresent(){
+  toPresent() {
     super.toPresent();
-    console.log(`My name is ${this.name},I work as ${this.job}, programing in ${this.programingLanguage} and I make $ ${this.salary} a month.`)
+    console.log(
+      `My name is ${this.name},I work as ${this.job}, programing in ${this.programingLanguage} and I make $ ${this.salary} a month.`
+    );
   }
 }
 //instance for programer
-const programerInstance= new programer("Caio", "programer", 8000, "Dart");
-
-
+const programerInstance = new programer("Caio", "programer", 8000, "Dart");
 
 class designer extends employee {
-    constructor(name, age, job, salary, typeofDesign) {
-        super(name, age, job, salary);
-        this.typeofDesign = typeofDesign;
+  constructor(name, age, job, salary, typeofDesign) {
+    super(name, age, job, salary);
+    this.typeofDesign = typeofDesign;
   }
 
-  toPresent(){
+  toPresent() {
     super.toPresent();
-    console.log(`My name is ${this.name},I work as ${this.cargo}, specifically in ${this.typeofDesign} and I make $ ${this.salary} a month.`)
+    console.log(
+      `My name is ${this.name},I work as ${this.cargo}, specifically in ${this.typeofDesign} and I make $ ${this.salary} a month.`
+    );
   }
 }
 const designerInstance = new designer("Thomas", "designer", 4000, "UX");
 
 class manager extends employee {
-    constructor(name, age, job, salary, department) {
-        super(name, age, job, salary);
-        this.department = department;
+  constructor(name, age, job, salary, department) {
+    super(name, age, job, salary);
+    this.department = department;
   }
 
-  toPresent(){
+  toPresent() {
     super.toPresent();
-    console.log(`My name is ${this.name},I work as ${this.job}, specifically in ${this.departament} department and I make $ ${this.salary} a month.`)
+    console.log(
+      `My name is ${this.name},I work as ${this.job}, specifically in ${this.departament} department and I make $ ${this.salary} a month.`
+    );
   }
 }
 const managerDepartmentInstance = new manager("development");
